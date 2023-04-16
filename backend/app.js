e1fc8de1ef28c7aa3e48362949cf43b3e8407b66
Charles Cruse <charle9@vt.edu>
2023-04-16 17:30:13 -0400

    csurf

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+require('./models/User');
+
+const usersRouter = require('./routes/api/users');
+
 // Security Middleware
 if (!isProduction) {
   // Enable CORS only in development because React will be on the React
@@ -43,3 +47,97 @@ app.use('/api/tweets', tweetsRouter);
 app.use('/api/csrf', csrfRouter);
 
 module.exports = app;
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+  
+const serverErrorLogger = debug('backend:error');
+  
+  // Express custom error handler that will be called whenever a route handler or
+  // middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      statusCode,
+      errors: err.errors
+    })
+});
+  
+module.exports = app;
+
+router.post('/register', async (req, res, next) => {
+    // Check to make sure no one has already registered with the proposed email or
+    // username.
+    const user = await User.findOne({
+      $or: [{ email: req.body.email }, { username: req.body.username }]
+    });
+  
+    if (user) {
+      // Throw a 400 error if the email address and/or username already exists
+      const err = new Error("Validation Error");
+      err.statusCode = 400;
+      const errors = {};
+      if (user.email === req.body.email) {
+        errors.email = "A user has already registered with this email";
+      }
+      if (user.username === req.body.username) {
+        errors.username = "A user has already registered with this username";
+      }
+      err.errors = errors;
+      return next(err);
+    }
+});
+
+
+
+router.post('/register', async (req, res, next) => {
+    // Check to make sure no one has already registered with the proposed email or
+    // username.
+    const user = await User.findOne({
+      $or: [{ email: req.body.email }, { username: req.body.username }]
+});
+  
+if (user) {
+      // Throw a 400 error if the email address and/or email already exists
+      const err = new Error("Validation Error");
+      err.statusCode = 400;
+      const errors = {};
+      if (user.email === req.body.email) {
+        errors.email = "A user has already registered with this email";
+      }
+      if (user.username === req.body.username) {
+        errors.username = "A user has already registered with this username";
+      }
+      err.errors = errors;
+      return next(err);
+}
+  
+    // Otherwise create a new user
+const newUser = new User({
+      username: req.body.username,
+      email: req.body.email
+});
+  
+bcrypt.genSalt(10, (err, salt) => {
+      if (err) throw err;
+      bcrypt.hash(req.body.password, salt, async (err, hashedPassword) => {
+        if (err) throw err;
+        try {
+          newUser.hashedPassword = hashedPassword;
+          const user = await newUser.save();
+          return res.json({ user });
+        }
+        catch(err) {
+          next(err);
+        }
+      })
+    });
+});
+  
+module.exports = router;
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
